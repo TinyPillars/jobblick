c844350968cc671d4b5c78e75aafcbf215214c26
Marcus Peterson <marcus.peterson@yh.nackademin.se>
2024-06-17 20:35:11 +0200

    forgot some stuff

--- database_logic/tags_algorithm.py
+++ database_logic/tags_algorithm.py
@@ -1,100 +1,71 @@
-stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 
-'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 
-'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 
+stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours',
+'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself',
+'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself',
 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves',
- 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are',
- 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing',
- 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 
-'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 
-'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 
-'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 
-'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 
+'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are',
+'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing',
+'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for',
+'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to',
+'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once',
+'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most',
+'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very',
 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y',
- 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 
+'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't",
 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn',
- "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 
+"mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't",
 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
 
 from porter_stemmer import PorterStemmer
 
-
 p = PorterStemmer()
-class tagging_algoritm:
+
+class tagging_algorithm:
     def __init__(self, raw_text):
         self.text = raw_text
 
-    @staticmethod
     def preprocess_text(self):
-        lowercased = self.text
-        cleaned = lowercased.replace("."," ")
-        array = list(cleaned.split(" "))
-        tokenized = [sub.split() for sub in array]
-        return tokenized
-    
+        lowercased = self.text.lower()
+        cleaned = lowercased.replace(".", " ")
+        array = list(cleaned.split())
+        return array
+
     @classmethod
-    def remove_stop_words(cls,tokenized_text):
+    def remove_stop_words(cls, tokenized_text):
         global stopwords
-        stop_words = stopwords
-        filtered = []
-        for word in tokenized_text:
-            for j in word:
-                if j in stop_words:
-                    continue
-                elif word not in stop_words:
-                    filtered.append(word)
+        filtered = [word for word in tokenized_text if word not in stopwords]
         return filtered
-    
+
     @classmethod
-    def stemming(cls,filtered_text):
-        stemmed_text = []
-        for word in filtered_text:
-            for j in word:
-                stemmed_word = p.stem(j,0,len(j)-1)
-                stemmed_text.append(stemmed_word)
+    def stemming(cls, filtered_text):
+        stemmed_text = [p.stem(word, 0, len(word) - 1) for word in filtered_text]
         return stemmed_text
-    
+
     @classmethod
-    def function_analyze_frequency(cls,processed_text):
+    def function_analyze_frequency(cls, processed_text):
         frequency_dict = {}
-        for words in processed_text:
-            for word in words:
-                if word in frequency_dict:
-                    frequency_dict[word] += 1
-                else:
-                    frequency_dict[word] = 1
+        for word in processed_text:
+            if word in frequency_dict:
+                frequency_dict[word] += 1
+            else:
+                frequency_dict[word] = 1
         return frequency_dict
-    
+
     @classmethod
-    def frequency_counter(cls,words):
-        mapped = {}
-        for i in words:
-            for j in i:
-                if j not in mapped:
-                    mapped[j] = 1    
-                else:
-                    mapped[j] += 1
-        return mapped
-    
-    @classmethod
-    def select_top_words(cls,frequency_dict,num_tags): #Num tags is arbitrary, but for example, we can choose how many tags we want  
-        sorted_words = tagging_algoritm.frequency_counter(frequency_dict)
-        top_words = sorted_words[:num_tags]
+    def select_top_words(cls, frequency_dict, num_tags):
+        sorted_words = sorted(frequency_dict.items(), key=lambda x: x[1], reverse=True)
+        top_words = [word for word, freq in sorted_words[:num_tags]]
         return top_words
-    
-    def generate_tags(self,nums_tags):
-        tokenized_text = tagging_algoritm.preprocess_text(self=self)
-        filtered_text = tagging_algoritm.remove_stop_words(tokenized_text=tokenized_text)
-        processed_text = tagging_algoritm.stemming(filtered_text=filtered_text)
-        frequency_dict = tagging_algoritm.function_analyze_frequency(processed_text=processed_text)
-        top_tags = tagging_algoritm.select_top_words(frequency_dict=frequency_dict,num_tags=nums_tags)
+
+    def generate_tags(self, num_tags):
+        tokenized_text = self.preprocess_text()
+        filtered_text = tagging_algorithm.remove_stop_words(tokenized_text)
+        processed_text = tagging_algorithm.stemming(filtered_text)
+        frequency_dict = tagging_algorithm.function_analyze_frequency(processed_text)
+        top_tags = tagging_algorithm.select_top_words(frequency_dict, num_tags)
         return top_tags
 
-
-
-
-
-#Example way of using
-"""long_string = (
+# Sample long string
+long_string = (
     "This is a very long string. This string is designed to be long and contains some comprehensible text. "
     "The purpose of this long string is to repeat certain words. Words like 'long', 'string', and 'text' are "
     "repeated many times to demonstrate the length and repetition. In this string, you will find that the word "
@@ -106,10 +77,7 @@ class tagging_algoritm:
     "demonstrates repetition. This repetition makes the string longer and emphasizes certain words. When you see "
     "a long string like this, you can understand how repetition works in a text. This is the end of the long string."
 )
-"""
 
-#time_to_tag = tagging_algoritm(long_string)
-
-#print(time_to_tag.generate_tags(nums_tags=4))
-
-
+# Create an instance of the tagging algorithm class and generate tags
+time_to_tag = tagging_algorithm(long_string)
+print(time_to_tag.generate_tags(num_tags=4))
