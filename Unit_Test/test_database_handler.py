f0902c45949743d8f0bfc126fa12e3e4ce413f92
suecoalto <suecoalto@gmail.com>
2024-07-07 22:01:18 +0200

    The unit test setup
    In the setup i choose to use pytest becuase of its simplicity.
    We use unitest.mock librar to mock interactions with external dependencies like
    MongoDB and MySQL. We do this to make sure that our test are isolated from
    live produktion.
    
    Explanation of the Provided Code:
    - Testing Valid Data:
    	• We create a dictionary valid_data with valid inputs.
    	• We create an instance of InsertData using valid_data and assert
    	that the fields are correctly assigned.
    
    - Testing Invalid thread_text:
    	• We create a copy of valid_data and modify thread_text to be too short.
    	• We use pytest.raises(ValidationError) to ensure that a ValidationError is
    	raised when we try to create an instance of InsertData with invalid thread_text.
    
    - Testing Invalid company_profile:
    	• We create a copy of valid_data and modify company_profile to have multiple words without hyphen.
    	• We use pytest.raises(ValidationError) to ensure that a ValidationError is
    	raised when we try to create an instance of InsertData with invalid company_profile.

--- Unit_Test/test_database_handler.py
+++ Unit_Test/test_database_handler.py
@@ -6,3 +6,31 @@ from unittest.mock import patch, MagicMock
 
 from database_logic.database_handler import InsertData, MySQLHandler, MongoDatabaseHandler
 
+#Test InsertDara Validation
+def test_insert_data_validation():
+	# Test valid data
+	valid_data = {
+		"username": "testuser1",
+		"email": "testuser1@test.com",
+		"thread_text": "This is a valid thread text with more than 500 characters" + "a" * 500,
+		"Category": "Jobb",
+		"company_profile": "Valid-company",
+    }
+	data = InsertData(**valid_data)
+	assert data.username == "testuser1"
+	assert data.email == "testuser1@test.com"
+	
+    # Test Invalid thread text that is to short
+	with pytest.raises(ValidationError):
+		invalid_data = valid_data.copy()
+		invalid_data["thread_text"] = "Too Short"
+		InsertData(**invalid_data)
+		
+    # Test invalid company_profile (in this case, multiple words without hyphen '-')
+	with pytest.raises(ValidationError):
+		invalid_data = valid_data.copy()
+		invalid_data["company:profile"] = "Invalid Company"
+		InsertData(**invalid_data)
+		
+
+
